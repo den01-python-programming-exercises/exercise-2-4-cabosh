ebafaea10e343b0da61d46419440990060bcfb33
cabosh <callum.bosher@gmail.com>
2020-06-04 22:15:08 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,15 @@
 def main():
     #write your code below this line
 
+    num1 = int(input("Enter your first number: "))
+    num2 = int(input("Enter your second number: "))
+
+    if num1 > num2:
+        print(f"{num1} is greater than {num2}.")
+    elif num1 < num2:
+        print(f"{num1} is smaller than {num2}.")
+    else:
+        print(f"{num1} is equal to {num2}.")
+
 if __name__ == '__main__':
     main()
